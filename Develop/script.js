70f47cf82545fc797de8c7e69bae8440f6b1a01e
Michael Silva <msilva@aggienetwork.com>
2020-04-19 18:15:32 -0500

    added dynamic background color to tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,18 @@
-var tasks = {};
+// save tasks to empty array
+var tasks = [];
+var currentTime =moment().hour();
 
+// load tasks from local storage
 var loadSchedule = function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
 };
 
+// save tasks to local storage
 var saveSchedule = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// edit p element on click inside scheduler
 $('.text-area').on("click", "p", function() {
     var text = $(this)
     .text()
@@ -31,8 +36,122 @@ $('.text-area').on("blur", ".text-area", function() {
     $(this).replaceWith(workP);
 });
 
-$("#save").on("click", function() {
+// save task when user clicks button with save id tag
+$('.saveBtn').on("click", function() {
+    console.log("saved");
     saveSchedule();
 });
 
-loadSchedule();
+// change background color of tasks depending on time of day
+
+
+
+$(".time-block").each(function() {
+    if (currentTime === 9) {
+        $("#9").addClass("current");
+    }
+    else if (currentTime > 9) {
+        $("#9").addClass("past");
+    }
+    else if (currentTime < 9) {
+        $("#9").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 10) {
+        $("#10").addClass("current");
+    }
+    else if (currentTime > 10) {
+        $("#10").addClass("past");
+    }
+    else if (currentTime < 10) {
+        $("#10").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 11) {
+        $("#11").addClass("current");
+    }
+    else if (currentTime > 11) {
+        $("#11").addClass("past");
+    }
+    else if (currentTime < 11) {
+        $("#11").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 12) {
+        $("#12").addClass("current");
+    }
+    else if (currentTime > 12) {
+        $("#12").addClass("past");
+    }
+    else if (currentTime < 12) {
+        $("#12").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 1) {
+        $("#1").addClass("current");
+    }
+    else if (currentTime > 1) {
+        $("#1").addClass("past");
+    }
+    else if (currentTime < 1) {
+        $("#1").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 2) {
+        $("#2").addClass("current");
+    }
+    else if (currentTime > 2) {
+        $("#2").addClass("past");
+    }
+    else if (currentTime < 2) {
+        $("#2").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 3) {
+        $("#3").addClass("current");
+    }
+    else if (currentTime > 3) {
+        $("#3").addClass("past");
+    }
+    else if (currentTime < 3) {
+        $("#3").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 4) {
+        $("#4").addClass("current");
+    }
+    else if (currentTime > 4) {
+        $("#4").addClass("past");
+    }
+    else if (currentTime < 4) {
+        $("#4").addClass("future");
+    }
+});
+
+$(".time-block").each(function() {
+    if (currentTime === 5) {
+        $("#5").addClass("current");
+    }
+    else if (currentTime > 5) {
+        $("#5").addClass("past");
+    }
+    else if (currentTime < 5) {
+        $("#5").addClass("future");
+    }
+});
+
+ loadSchedule();
